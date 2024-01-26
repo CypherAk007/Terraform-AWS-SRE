f1570ad80fb5414f97fd5b928db3cb1364e3e489
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-26 11:13:13 +0530

    AMI FROM DATA SOURCE

--- 01_Terraform/03_Terraform_Input_Output/c2-versions.tf
+++ 01_Terraform/03_Terraform_Input_Output/c2-versions.tf
@@ -1,3 +1,35 @@
+# What are data sources?
+# - Bring information outside terraform to inside
+# - get info of other tf config
+
+# AMI Data source
+data "aws_ami" "amxLinux2" {
+  most_recent      = true
+  owners           = ["amazon"]
+# How to get ami name : console->copy ami id from launch instance
+# -> goto AMI->search for AMI ID= ${ami id} copy the name 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+#   make sure you have Architecture 64-bit (x86)?
+    filter {
+      name="architecture"
+      values = ["x86_64"]
+    }
+
+}
+
 # resource "aws_instance" "myec2"{
 #     ami = "ami-0a3c3a20c09d6f377"
 #     instance_type = "t2.micro"
