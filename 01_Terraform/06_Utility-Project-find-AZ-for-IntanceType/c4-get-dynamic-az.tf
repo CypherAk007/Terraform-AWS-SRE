d0e4a489d653e9b2e491b383a81deaafd542d9f5
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-01-29 15:03:29 +0530

    completed Utility Project - get az's as per instance type provided

--- 01_Terraform/06_Utility-Project-find-AZ-for-IntanceType/c4-get-dynamic-az.tf
+++ 01_Terraform/06_Utility-Project-find-AZ-for-IntanceType/c4-get-dynamic-az.tf
@@ -1,6 +1,16 @@
+# 1->Get list of az in a spefic region (region in us-east-1)
+# DataSource-1
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 # DataSource
 data "aws_ec2_instance_type_offerings" "my_ins_type" {
-  for_each = toset(["us-east-1a","us-east-1b","us-east-1e"])
+#   for_each = toset(["us-east-1a","us-east-1b","us-east-1e"])
+  for_each = toset(data.aws_availability_zones.my_azones.names)
   filter {
     name   = "instance-type"
     values = [ "t3.micro"]
@@ -14,31 +24,45 @@ data "aws_ec2_instance_type_offerings" "my_ins_type" {
   location_type = "availability-zone"
 }
 
-
-# Output of for each is in for loop
-output "output_v_final_1" {
-#   value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
-    value = toset([for t in data.aws_ec2_instance_type_offerings.my_ins_type: t.instance_types])
-}
-# + output_v2_1 = [
-#       + [
-#           + "t3.micro",
-#         ],
-#       + [],
-#     ]
-
-# OUtput map az-> instance_type
-output "output_v_final_2" {
+# Gives list of az with instancetype in it if that az is avail... or empty list 
+output "all_az_mapped_Instance_types" {
   value = {
-    for az,details in data.aws_ec2_instance_type_offerings.my_ins_type: az=>details.instance_types
+    for az,details in data.aws_ec2_instance_type_offerings.my_ins_type:
+    az=>details.instance_types 
   }
 }
-# + output_v2_2 = {
-#       + us-east-1a = [
-#           + "t3.micro",
-#         ]
-#       + us-east-1b = [
-#           + "t3.micro",
-#         ]
-#       + us-east-1e = []
 
+# Gives list of az with instancetype in it if that az is avail...
+output "all_az_mapped_Instance_types_only_accepted_az" {
+  value = {
+    for az,details in data.aws_ec2_instance_type_offerings.my_ins_type:
+    az=>details.instance_types if length(details.instance_types)!=0
+  }
+}
+
+
+# Gives list of az with instancetype in it if that az is avail... **List Format**
+# using Keys()
+output "all_az_mapped_Instance_types_only_accepted_az_list" {
+  value = keys({
+    for az,details in data.aws_ec2_instance_type_offerings.my_ins_type:
+    az=>details.instance_types if length(details.instance_types)!=0
+  })
+}
+
+# pick 1st val fm list 
+output "all_az_mapped_Instance_types_only_accepted_az_list_1st_val" {
+  value = keys({
+    for az,details in data.aws_ec2_instance_type_offerings.my_ins_type:
+    az=>details.instance_types if length(details.instance_types)!=0
+  })[0]
+}
+
+# o/p->
+# + all_az_mapped_Instance_types_only_accepted_az_list         = [
+#       + "us-east-1a",
+#       + "us-east-1b",
+#       + "us-east-1c",
+#       + "us-east-1d",
+#       + "us-east-1f",
+#     ]
